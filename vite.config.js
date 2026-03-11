1eae15e3d465d46b2b24a0f992f4e1ec6dc5f61e
Sunset2324 <146049853+Sunset2324@users.noreply.github.com>
2026-03-12 00:14:46 +0700

    Update vite.config.js
    chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,21 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// Source - https://stackoverflow.com/a/69261966
+// Posted by Haseeb Saeed, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-03-12, License - CC BY-SA 4.0
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  base: "/Stakepool-Frontend/",
+  plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "node_modules"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
